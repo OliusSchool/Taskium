b5384bb8a498e2b76aa7f1cddf8298f8b7e6d57c
OliusSchool <nonu14013@gmail.com>
2026-04-12 01:53:05 +0000

    idk

--- GUI/Categories.lua
+++ GUI/Categories.lua
@@ -4,7 +4,27 @@ if not TaskUI then
 	error("TaskUI was not loaded before Categories.lua")
 end
 
-TaskUI:CreateCategory({
+TaskAPI:CreateCategory({
+	Name = "Combat",
+	Position = UDim2.new(0.1, 0, 0.2, 0)
+})
+
+TaskAPI:CreateCategory({ 
+	Name = "Movement",
+	Position = UDim2.new(0.3, 0, 0.2, 0)
+})
+
+TaskAPI:CreateCategory({ 
+	Name = "Render",
+	Position = UDim2.new(0.5, 0, 0.2, 0)
+})
+
+TaskAPI:CreateCategory({ 
+	Name = "Player",
+	Position = UDim2.new(0.7, 0, 0.2, 0)
+})
+
+TaskAPI:CreateCategory({ 
 	Name = "Other",
 	Position = UDim2.new(0.9, 0, 0.2, 0)
 })
