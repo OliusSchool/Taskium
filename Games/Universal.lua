4209449cfead278abe420bffff48346c3efa7ab4
OliusSchool <nonu14013@gmail.com>
2026-04-13 01:17:34 +0000

    dingus

--- Games/Universal.lua
+++ Games/Universal.lua
@@ -48,7 +48,7 @@ SilentAima = TaskAPI.Categories.Combat:CreateModule({
 
 local SilentAimab
 SilentAimab = TaskAPI.Categories.Movement:CreateModule({
-	Name = "notspeed",
+	Name = "speed",
 	Function = function(callback)
 		print(callback, "module state")
 
