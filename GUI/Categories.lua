f501d4d0228a631d907f852196a68ec129ee9745
OliusSchool <nonu14013@gmail.com>
2026-04-13 23:28:44 +0000

    wow

--- GUI/Categories.lua
+++ GUI/Categories.lua
@@ -6,27 +6,27 @@ end
 
 TaskAPI:CreateCategory({
 	Name = "Combat",
-	Position = UDim2.new(0.07, 0, 0.2, 0)
+	Position = UDim2.new(0.1, 0, 0.2, 0)
 })
 
 TaskAPI:CreateCategory({ 
 	Name = "Movement",
-	Position = UDim2.new(0.17, 0, 0.2, 0)
+	Position = UDim2.new(0.25, 0, 0.2, 0)
 })
 
 TaskAPI:CreateCategory({ 
 	Name = "Render",
-	Position = UDim2.new(0.27, 0, 0.2, 0)
+	Position = UDim2.new(0.35, 0, 0.2, 0)
 })
 
 TaskAPI:CreateCategory({ 
 	Name = "Player",
-	Position = UDim2.new(0.37, 0, 0.2, 0)
+	Position = UDim2.new(0.45, 0, 0.2, 0)
 })
 
 TaskAPI:CreateCategory({ 
 	Name = "Other",
-	Position = UDim2.new(0.47, 0, 0.2, 0)
+	Position = UDim2.new(0.55, 0, 0.2, 0)
 })
 
 return TaskAPI
